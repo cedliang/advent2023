68ad0f7eb4e88f83ad80b68beed0ed309d8331ac
cedliang <62864170+cedliang@users.noreply.github.com>
2023-12-02 15:15:40 +1100

    day1

---
+++ src/main1/main1a.hs
@@ -0,0 +1,11 @@
+import Data.Char
+
+main :: IO ()
+main = do
+  fileLines <- words <$> readFile "src/main1/Day1.txt"
+  let digits = map (filter isDigit) fileLines
+  let sums = map sumChars digits
+  print $ sum sums
+
+sumChars :: String -> Int
+sumChars xs = 10 * read [head xs] + read [last xs]
---
+++ src/main1/main1b.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Char
+import Data.Text
+import Data.Text.IO
+import Prelude hiding (filter, head, last, readFile, words)
+
+main :: IO ()
+main = do
+  fileLines <- words <$> readFile "src/main1/Day1.txt"
+  let digits = filter isDigit . transformText <$> fileLines
+  let sums = sumChars <$> digits
+  print $ sum sums
+
+sumChars :: Text -> Int
+sumChars xs = 10 * read [head xs] + read [last xs]
+
+transformText :: Text -> Text
+transformText =
+  replace "one" "o1e"
+    . replace "two" "t2o"
+    . replace "three" "t3e"
+    . replace "four" "f4r"
+    . replace "five" "f5e"
+    . replace "six" "s6x"
+    . replace "seven" "s7n"
+    . replace "eight" "e8t"
+    . replace "nine" "n9e"
